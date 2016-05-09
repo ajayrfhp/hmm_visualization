fba0e6921aa8b81e1562ef80eebc72580ce39f85
ajayrfhp <ajayrfhp1710@gmail.com>
2016-05-10 00:33:17 +0530

    stuck in animation

--- viterbi.js
+++ viterbi.js
@@ -1,19 +1,47 @@
+function sleep(time){
+	var stopping_time = new Date().getTime();
+	while( new Date().getTime < stop + time ){
+		;
+	}
+}
+function animate(this_node,this_text,layers,i,j){
+
+	var previous_color = this_node.attr("fill");
+	this_node.attr("fill","black");
+	var delay = 1000;
+	var completed = false;
+
+	setTimeout(function(){
+			this_node.attr("fill",previous_color);
+			this_text.text(function(d){
+				return layers[i].nodes[j].probability.toFixed(3);
+			});
+			completed = true;
+		},delay);
+	
+
+}
 function viterbi_compute(data,layers,observed_sequence){
 	
 	for( i = 0; i < layers.length; i++ ){
 					if( i == 0 ){
 						probability = 0;
+						// Color i th node;
+						animate(d3.select("#c_"+i),d3.select("#t_"+i),layers,i,0);
+						// put a delay
 					}
 					else if( i == 1 ){
 						for( j = 0; j < layers[i].cnt; j++ ){
 							var this_state = layers[i].nodes[j].state;
 							layers[i].nodes[j].probability = data.start_probability[this_state] * data.emission_probability[this_state][observed_sequence[i-1]]; 	
+							var cnt = 1 + (i-1)*layers[i].cnt + j;
+							console.log(cnt);
+							animate(d3.select("#c_"+cnt),d3.select("#t_"+cnt),layers,i,j);
 						}
 					}
 					else{
 						// DP EQUATION OH WOW OH WOW
 						var best_probability = 0;
-						
 						for( j = 0; j < layers[i].cnt; j++ ){
 							best_probability = 0;
 							var this_state = layers[i].nodes[j].state;
