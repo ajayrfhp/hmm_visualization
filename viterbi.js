309733b7f66478f730d2e34aa5864255c4f44905
ajayrfhp <ajayrfhp1710@gmail.com>
2016-05-10 19:34:01 +0530

    started viterbi with recursion

--- viterbi.js
+++ viterbi.js
@@ -17,22 +17,36 @@ function animate_edge(i,j,k){
 	d3.select(edge).transition().delay(delay).attr("style","stroke: rgb(62, 167, 138);stroke-width:5");
 	setTimeout(function(){
 			d3.select(edge).attr("style","stroke: rgb(62, 167, 138);stroke-width:2");
-		},delay + 1000  );
-	 
-
-
+		},delay+500);
 }
-function animate(this_node,this_text,layers,i,j){
+function animate(this_node,this_text,layers,i,j,data){
 	var previous_color = this_node.attr("fill");
 	this_node.attr("fill","0D0E04");
 	var delay = (i+1) * 2000 + j *1000;
+	console.log(i,j,delay);
 	var completed = false;
+	var this_state = layers[i].nodes[j].state;
+
+	// HIGHLIGHT THIS STATE IN THE TABLE
+
+	var row_number = null;
+	if( i!=0 )
+		{
+			row_number = data.states.indexOf(this_state) + 2;
+			d3.select("tr:nth-child(" + row_number + ")" ).transition().delay(delay).attr("style","background-color:black;color:white");
+		}
+
 
 	setTimeout(function(){
-			this_node.attr("fill",previous_color);
 			this_text.text(function(d){
 				return layers[i].nodes[j].probability.toFixed(3);
-			});
+			});		
+			this_node.attr("fill",previous_color);
+		
+			if( i!=0 ){
+				d3.select("tr:nth-child(" + row_number + ")" ).attr("style","background-color:white;color:black");
+			}	
+
 			completed = true;
 		},delay);
 	 
@@ -42,7 +56,7 @@ function viterbi_compute(data,layers,observed_sequence){
 					if( i == 0 ){
 						probability = 0;
 						// Color i th node;
-						animate(d3.select("#c_"+i),d3.select("#t_"+i),layers,i,0);
+						animate(d3.select("#c_"+i),d3.select("#t_"+i),layers,i,0,data);
 
 						// put a delay
 					}
@@ -51,8 +65,10 @@ function viterbi_compute(data,layers,observed_sequence){
 							var this_state = layers[i].nodes[j].state;
 							layers[i].nodes[j].probability = data.start_probability[this_state] * data.emission_probability[this_state][observed_sequence[i-1]]; 	
 							var cnt = 1 + (i-1)*layers[i].cnt + j;
-							animate(d3.select("#c_"+cnt),d3.select("#t_"+cnt),layers,i,j);
+							animate(d3.select("#c_"+cnt),d3.select("#t_"+cnt),layers,i,j,data);
+							
 							animate_edge(i,j,0);
+
 						}
 					}
 					else{
@@ -71,7 +87,7 @@ function viterbi_compute(data,layers,observed_sequence){
 							}
 							layers[i].nodes[j].probability = best_probability;
 							var cnt = 1 + (i-1)*layers[i].cnt + j;
-							animate(d3.select("#c_"+cnt),d3.select("#t_"+cnt),layers,i,j);
+							animate(d3.select("#c_"+cnt),d3.select("#t_"+cnt),layers,i,j,data);
 						}
 						best_probability = 0;
 						var temp_i, temp_j;
@@ -104,4 +120,20 @@ function viterbi_compute(data,layers,observed_sequence){
 
 				answer.push(best_state);
 	return answer			
+}
+
+function viterbi_compute_reverse(data,layers,observed_sequence,current_layer){
+
+	// HIGHLIGHT INCOMING EDGES FROM PREVIOUS LAYER, CHANGE COLOR OF THIS NODE, INSIDE SITEMOUT GIVE RECURSION CALL FOR NEXT LAYER
+	if( current_layer == 0){
+		// NO INCOMING EDGES FROM PREVIOUS LAYER
+		
+
+	}
+	else if( current_layer == layers.length-1 ){
+
+	}
+	else{
+
+	}
 }
